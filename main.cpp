6ca52fbe885595e5d9846c56779eb30501e94469
Ryotaro Suzuki <rsuzuki@diverta.co.jp>
2016-10-04 16:31:07 +0900

    デコーダーできた

--- main.cpp
+++ main.cpp
@@ -5,34 +5,38 @@
 #include "Eigen/Geometry"
 #include "Eigen/src/KroneckerProduct/KroneckerTensorProduct.h"
 #include "time.h"
+#include <chrono>
+#include <iomanip>
 
 using namespace std;
 using namespace Eigen;
 
-#define PRINT(X) cout << #X << ":\n" << X << endl << endl;
+#define PRINT(X) cout << #X << ":\n" << setprecision(10) << X << endl << endl;
 
 //params
-const int  N=1024;
+const int  N=2;
 const int  K=2;
-const double e = 0.5f;
+const float e = 0.5f;
 const int A[] ={2,4};
 
 int vsize(VectorXi &x);
-double calcW(int y, int x);
+long double calcW(int y, int x);
 VectorXi index_o(VectorXi &x);
 VectorXi index_e(VectorXi &x);
 VectorXi retBinary(VectorXi &x);
-double calcW_i(int i, int n, VectorXi &u, VectorXi &u_i, VectorXi &y);
+long double calcW_i(int i, int n, VectorXi &u, VectorXi &u_i, VectorXi &y);
 VectorXi encoder(int n, VectorXi &input);
 VectorXi channel(VectorXi &input);
 VectorXi decoder(VectorXi &input, VectorXi &u_Ac);
+VectorXi generateUi(VectorXi &x,VectorXi &u_Ac);
+
 
 int vsize(VectorXi &x){
     return (x.array() >= 0).count();
 }
 
-double calcW(int y, int x) {
-    double retVal = 0.0f;
+long double calcW(int y, int x) {
+    long double retVal = 0.0f;
     if (x == y) {
         retVal = 1 - e;
     } else if (y == 2) {
@@ -71,15 +75,15 @@ VectorXi retBinary(VectorXi &x) {
     return ret;
 }
 
-double calcW_i(int i, int n, VectorXi &u, int u_i, VectorXi &y) {
-    double W_i = 0.0;
+long double calcW_i(int i, int n, VectorXi &u, int u_i, VectorXi &y) {
+    long double W_i = 0.0;
 
     if ( n == 2 ){
         if ( i == 1 ) {
-            W_i = 0.5 * (calcW(y[0] ,(u[0]+0) % 2) * calcW(y[1],0)
-                    + calcW(y[0] ,(u[0]+1) % 2) * calcW(y[1],1));
+            W_i = 0.5 * (calcW(y[0] ,(u_i+0) % 2) * calcW(y[1],0)
+                    + calcW(y[0] ,(u_i+1) % 2) * calcW(y[1],1));
         } else {
-            W_i = 0.5 * calcW(y[0] ,(u[0]+u[1]) % 2) * calcW(y[1],u[1]);
+            W_i = 0.5 * calcW(y[0] ,(u[0]+u_i) % 2) * calcW(y[1],u_i);
         }
     } else {
         VectorXi tempY1(vsize(y)/2);
@@ -108,7 +112,7 @@ double calcW_i(int i, int n, VectorXi &u, int u_i, VectorXi &y) {
                       * calcW_i( i/2   , n/2, u_e       , u_i            , tempY2);
         }
     }
-    cout << "W_" << "n:" << n << ",i:" << i << endl;
+//    cout << "W_" << "n:" << n << ",i:" << i << endl;
 //    PRINT(W_i);
     return W_i;
 }
@@ -128,7 +132,7 @@ VectorXi encoder(int n, VectorXi &input){
         v_n[n/2 + i] = s_n[2*i +1];
     }
 
-    if(n == 2){
+    if (n == 2) {
         x_n[0] = (v_n[0] + v_n[1]) % 2;
         x_n[1] = v_n[1];
 
@@ -164,85 +168,110 @@ VectorXi channel(VectorXi &input){
 
     for (int i = 0; i < N; i++) {
         y[i] = input[i];
-        if((double)rand() / RAND_MAX < e){
+        if((long double)rand() / RAND_MAX < e){
             y[i] = 2;
         }
     }
     return y;
 }
 
-VectorXi decoder(VectorXi &input, VectorXi &u_Ac){
-//    VectorXd w_n_i(N);
-//    VectorXd w_n = VectorXd::Constant(N,e);
-//    VectorXd h_i(N);
+VectorXi decoder(VectorXi &input, VectorXi &u, VectorXi &u_Ac){
+
+    VectorXd h_i(N);
     VectorXi u_n_est(N);
-//
-//    //h_i計算
-//    for (int i = 0; i < N; i++) {
-//        int count = 0;
-//        for (int j = i; j < N; j++) {
-//            cout << input[j] << endl;
-//            if( input[j] == 0 || input[j] == 1 ) {
-//                count++;
-//            }
-//        }
-//        w_n_i =  count * w_n / (pow(2, N-1));
-//        double llr = 1; //w_n_i[i]  / w_n_i[i];
-//        if (llr >= 1) {
-//            h_i[i] = 0;
-//        } else {
-//            h_i[i] = 1;
-//        }
-//    }
-//
-//    cout << "w_n_i" << endl;
-//    cout << w_n_i << endl;
-//
-//    cout << "h_i" << endl;
-//    cout << h_i << endl;
-//
-////
-////    //u_n_est計算
-////    for (int i = 0; i < N; i++) {
-////        int in_A = 0;
-////        for (int j = 0; j < K; j++) {
-////            if ( i == A[j]) {
-////                in_A = 1;
-////            }
-////        }
-////
-////        // Aに含まれるindexなら既知
-////        if (in_A == 1) {
-////            u_n_est[i] = u_Ac[i];
-////        } else {
-////            u_n_est[i] = h_i[i];
-////        }
-////    }
-//
+
+    //h_i計算
+    for (int i = 0; i < N; i++) {
+        int count = 0;
+        for (int j = i; j < N; j++) {
+            if( input[j] == 0 || input[j] == 1 ) {
+                count++;
+            }
+        }
+        long double llr = calcW_i(i, N, u, 0, input)
+                   / calcW_i(i, N, u, 1, input);
+        PRINT(calcW_i(i, N, u, 0, input));
+        PRINT(calcW_i(i, N, u, 1, input));
+        if (llr >= 1) {
+            h_i[i] = 0;
+        } else {
+            h_i[i] = 1;
+        }
+    }
+    PRINT(h_i);
+
+    //u_n_est計算
+    for (int i = 0; i < N; i++) {
+        int in_A = 0;
+        int index = 0;
+        for (int j = 0; j < K; j++) {
+            if ( i+1 == A[j]) {
+                in_A = 1;
+                index = j;
+            }
+        }
+
+        // Aに含まれるindexなら既知
+        if (in_A == 1) {
+            u_n_est[i] = u_Ac[index];
+        } else {
+            u_n_est[i] = h_i[i];
+        }
+    }
+
     return u_n_est;
 }
 
-int main(void) {
+VectorXi generateUi(VectorXi &x,VectorXi &u_Ac){
+    VectorXi ret(N);
     srand((int) time(NULL));
-//    VectorXi u_Ac(K);
-//    u_Ac << 1, 0;
-    VectorXi u_n(N);
-    for(int i=0; i<N;i++){
-        u_n[i] = rand() % 2;
+    for (int i = 0; i < N; i++) {
+        int in_A = 0;
+        int index = 0;
+        for (int j = 0; j < K; j++) {
+            if ( i+1 == A[j]) {
+                in_A = 1;
+                index = j;
+            }
+        }
+        // Aに含まれるindexなら既知
+        if (in_A == 1) {
+            ret[i] = u_Ac[index];
+        } else {
+            ret[i] = rand() % 2;
+        }
     }
-//    u_n << 1,1,1,0,1,1,1,0;  //input
+    return ret;
+}
+
+int main(void) {
+    VectorXi u_Ac(K);
+    u_Ac << 1, 0;
+    VectorXi u_n(N);
+    u_n = generateUi(u_n, u_Ac);
     PRINT(u_n);
-
-
     VectorXi x_n = encoder(N, u_n);
     VectorXi y_n = channel(x_n);
     PRINT(x_n);
     PRINT(y_n);
 
-    int i = 1024;
-    double W_i = calcW_i(i, N, u_n, u_n[i-1], y_n);
-    PRINT(W_i);
-    //VectorXi u_n_est = decoder(y_n,u_Ac);
+    const auto startTime = chrono::system_clock::now();
+
+    int i = 1;
+//    long double W_i = calcW_i(i, N, x_n, x_n[i-1], y_n);
+//    PRINT(W_i);
+
+    long double llr_0 = calcW_i(i, N, u_n, 0, y_n);
+    long double llr_1 = calcW_i(i, N, u_n, 1, y_n);
+    PRINT(llr_0);
+    PRINT(llr_1);
+
+    VectorXi u_n_est = decoder(y_n, x_n, u_Ac);
+    PRINT(u_n_est);
+
+    const auto endTime = chrono::system_clock::now();
+    const auto timeSpan = endTime - startTime;
+    cout << "処理時間:" << chrono::duration_cast<chrono::milliseconds>(timeSpan).count() << "[ms]" << endl;
     
     return 0;
 }
